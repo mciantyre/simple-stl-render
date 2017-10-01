79f2d52a5ed454a9f5995a555f3328333734858b
Ian McIntyre <ianpmcintyre@gmail.com>
2017-10-01 17:08:33 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Demo STL renderer
+
+A demonstration of rendering a STL file in the browser using [three.js](https://threejs.org/).
+
+- left-click-and-hold: rotate
+- right-click-and-hold: pan
+- mouse wheel: zoom
+
+![STL rendered in browser](./docs/render.png)
+
+### Running locally
+
+```
+npm install
+npm run serve
+```
---
+++ src/index.js
@@ -0,0 +1,73 @@
+import * as THREE from 'three';
+import STLLoader from "./STLLoader";
+import OrbitControls from "./OrbitControls";
+import CuteOcto from "../models/CuteOcto.stl";
+
+// Prepare loaders and controls
+STLLoader(THREE);
+OrbitControls(THREE);
+
+//
+// Scene
+//
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xc2d8f9 );
+
+//
+// Camera
+//
+const camera = new THREE.PerspectiveCamera( 35, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+
+//
+// Renderer
+//
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+//
+// Lights
+//
+const bright = new THREE.DirectionalLight( 0xffffff );
+bright.position.set( 1, 1, 1 );
+scene.add( bright );
+const secondary = new THREE.DirectionalLight( 0xaaaaaa );
+secondary.position.set( -1, -1, -1 );
+scene.add( secondary );
+const ambient = new THREE.AmbientLight( 0x666666 );
+scene.add( ambient );
+
+//
+// Controls
+//
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableZoom = true;
+controls.enablePan = true;
+controls.enableRotate = true;
+
+//
+// STL Loader
+//
+const loader = new THREE.STLLoader();
+loader.load( CuteOcto, function ( geometry ) {
+  const material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 50 } );
+  const mesh = new THREE.Mesh( geometry, material );
+  mesh.rotation.set( - Math.PI / 2, 0, - Math.PI );
+  mesh.position.set( 0, - 0.5, 0 );
+  mesh.scale.set( 0.02, 0.02, 0.02 );
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
+  scene.add( mesh );
+});
+
+//
+// Animation loop
+//
+const animate = function () {
+  requestAnimationFrame( animate );
+  controls.update();
+  renderer.render( scene, camera );
+};
+
+animate();
